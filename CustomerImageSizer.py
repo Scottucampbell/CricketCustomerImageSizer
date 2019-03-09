cbdc5f7996d5c969bbb38da7249ea44ad6961f28
Scottucampbell <scottcampbellu@gmail.com>
2019-03-09 13:40:23 -0400

    With Clear Button

--- CustomerImageSizer.py
+++ CustomerImageSizer.py
@@ -37,30 +37,34 @@ if __name__ == "__main__":
     #function to be called when mouse is clicked
     def printcoords(event):
         global i
+        global myCircle
         #outputting x and y coords to console
         print (event.x,event.y)
         pointx[i] = event.x
         pointy[i] = event.y
 
-        print (pointx)
-        print (pointy)
         python_green = "#476042"
         x1, y1 = (event.x - 5), (event.y - 5)
         x2, y2 = (event.x + 5), (event.y + 5)
-        canvas.create_oval(x1, y1, x2, y2, fill=python_green)
+        myCircle = canvas.create_oval(x1, y1, x2, y2, fill=python_green)
         if i == 1:
             canvas.create_line(pointx[0], pointy[0], pointx[1], pointy[1], fill="red")
         if i > 2:
             canvas.create_line(pointx[2], pointy[2], pointx[3], pointy[3], fill="red")
 
-
-
         i = i+1
+
+    def clear():
+        global i
+        global myCircle
+        i = 0
+        canvas.delete(myCircle)
+
+
+
     #mouseclick event
     canvas.bind("<Button 1>",printcoords)
-
-
-
-    Button(root, text='OK', command=root.destroy).pack()
+    Button(root, text='CLEAR', command=clear).pack()
+    Button(root, text='CLOSE', command=root.destroy).pack()
 
     root.mainloop()
